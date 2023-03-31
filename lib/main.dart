a1e18b2c748edbea226d66f314941471054d7b03
OonaPolsa <oona.polsa@student.lab.fi>
2023-03-31 11:20:53 +0300

    pikkujuttuja

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously
+// ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously, unused_field, avoid_print
 
 import 'dart:convert';
 import 'dart:math';
--- lib/screens/castncrew.dart
+++ lib/screens/castncrew.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_interpolation_to_compose_strings
+// ignore_for_file: prefer_interpolation_to_compose_strings, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 import 'package:movie_thing/constats/api_constats.dart';
