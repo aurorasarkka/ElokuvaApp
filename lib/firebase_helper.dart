f85c32f2db0d269d362fa3139e71672105f43aa4
OonaPolsa <oona.polsa@student.lab.fi>
2023-04-13 12:03:38 +0300

    firebase jotain jotain

--- lib/firebase_helper.dart
+++ lib/firebase_helper.dart
@@ -1,1 +1,10 @@
+// ignore_for_file: unused_import
 
+import 'package:firebase_database/firebase_database.dart';
+import 'package:movie_thing/models/movie.dart';
+import 'package:movie_thing/models/movieManager.dart';
+
+class FirebaseHelper {
+  final DatabaseReference firebaseRef =
+      FirebaseDatabase.instance.ref().child('movies');
+}
